ba1773bce672b6d51a71aa49b593058defe51a84
Max Lee <maxjerry0107@gmail.com>
2024-01-21 19:53:29 -0500

    added code blocks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
   import AutoCompleteInput from "$lib/AutoCompleteInput.svelte";
+
+  import HighlightSvelte, { LineNumbers } from "svelte-highlight";
+  import typescript from "svelte-highlight/languages/typescript";
+  import irBlack from "svelte-highlight/styles/ir-black";
+  const code = `
+  <script lang="ts">
+    import AutoCompleteInput from "$lib/AutoCompleteInput.svelte";
+    const colors = ["White", "Red", "Yellow", "Green", "Blue", "Black"];
+
+    let color: string;
+    function handleChange(e: Event): void {
+      console.log(color);
+    }
+  <\/script>
+
+  <AutoCompleteInput
+    name="color"
+    containerStyle="width:100px"
+    placeholder="Colors"
+    items={colors}
+    on:blur={handleChange}
+    bind:value={color}
+  />
+  `;
   const colors = ["White", "Red", "Yellow", "Green", "Blue", "Black"];
 
   let color: string;
@@ -8,12 +32,29 @@
   }
 </script>
 
-<AutoCompleteInput
-  name="color"
-  containerStyle="width:100px"
-  placeholder="Colors"
-  items={colors}
-  on:change={handleChange}
-  on:blur={handleChange}
-  bind:value={color}
-/>
+<svelte:head>
+  {@html irBlack}
+</svelte:head>
+
+<div class="container">
+  <HighlightSvelte language={typescript} {code} let:highlighted>
+    <LineNumbers {highlighted} />
+  </HighlightSvelte>
+
+  <br />
+
+  <AutoCompleteInput
+    name="color"
+    containerStyle="width:100px"
+    placeholder="Colors"
+    items={colors}
+    on:blur={handleChange}
+    bind:value={color}
+  />
+</div>
+
+<style>
+  .container {
+    width: 50%;
+  }
+</style>
